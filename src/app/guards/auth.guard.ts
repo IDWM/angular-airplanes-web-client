e505a7018f3968aeeb2af019009f4ac5d7c23792
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-12-08 02:01:20 -0300

    refactor: enhance auth guard with user feedback notifications

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,14 +1,16 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { ToastService } from '../services/toast.service';
 
 export const authGuard: CanActivateFn = (_route, _state) => {
   const router = inject(Router);
   const authService = inject(AuthService);
+  const toastService = inject(ToastService);
 
-  if (!authService.isAuthInitialized()) return false;
-  else if (!authService.isAuthenticated()) {
+  if (!authService.isAuthInitialized() || !authService.isAuthenticated()) {
     router.navigate(['/login']);
+    toastService.error('Necesitas iniciar sesión para acceder a esta sección');
     return false;
   }
 
