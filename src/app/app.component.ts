1990ebe2682c9fde76130c7df8f64f6a35c94748
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-12-08 00:31:05 -0300

    feat: implement base layout with navbar and footer components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FlowbiteService } from './services/flowbite.service';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
