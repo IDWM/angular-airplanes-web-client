b249f5f13b5166abf2c9c2fa83e2551f82c2571e
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-12-08 02:02:24 -0300

    feat: create minimal home component with welcome message

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  template: `
+    <div class="text-center">
+      <h1 class="text-4xl font-bold">Bienvenido</h1>
+    </div>
+  `,
+})
+export class HomeComponent {}
