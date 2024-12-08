61d4fd8df53d3e5f21355e6b7b1cc445784bbce7
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-12-08 02:02:00 -0300

    feat: handle post-logout navigation flow

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -11,4 +11,10 @@ import { AuthService } from '../../services/auth.service';
 })
 export class NavbarComponent {
   protected readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
 }
