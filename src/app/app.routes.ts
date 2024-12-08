923de55143cf3ff358e4ef4d46073acfd2cfc487
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-12-08 02:03:36 -0300

    feat: configure application routing with protected routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { authGuard } from './guards/auth.guard';
+import { AirplaneListComponent } from './pages/airplane-list/airplane-list.component';
+import { airplaneListResolver } from './resolvers/airplane-list.resolver';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '',
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'airplane',
+        component: AirplaneListComponent,
+        resolve: {
+          airplanes: airplaneListResolver,
+        },
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
