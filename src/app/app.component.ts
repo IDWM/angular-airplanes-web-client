e337990fdd7f9071789a2c91f0b20161a5449557
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-12-08 02:00:16 -0300

    feat: integrate toast component in app root layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { FlowbiteService } from './services/flowbite.service';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToastComponent } from './components/toast/toast.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, ToastComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
